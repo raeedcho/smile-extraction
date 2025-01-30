f6fc572103b14dceb3ca8fe339cda3794c61c1e6
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2025-01-30 10:37:26 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# smile-extraction
+
+a package to extract and process data collected from monkey host in the Batista lab
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="smile_extract",
+    packages=find_packages(),
+)
