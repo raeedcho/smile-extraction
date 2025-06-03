b764b27765a2bfe0af10ae4aa089aceaedfdede5
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2025-06-03 12:23:47 -0400

    refactor: remove task inference logic in favor of translating pure trial name information

--- smile_extract/trial_info.py
+++ smile_extract/trial_info.py
@@ -1,12 +1,11 @@
 from datetime import datetime
-import re
 import numpy as np
 import pandas as pd
 
 def get_trial_id(smile_trial: dict) -> int:
     return int(smile_trial['Overview']['trialNumber'].replace('Trial',''))
 
-def get_trial_datetime(smile_trial: dict) -> str:
+def get_trial_datetime(smile_trial: dict) -> pd.Timestamp:
     return pd.to_datetime(
         datetime.strptime(
             str(int(smile_trial['Overview']['date'])),
@@ -14,7 +13,7 @@ def get_trial_datetime(smile_trial: dict) -> str:
         ).strftime('%Y-%m-%d %H:%M:%S')
     )
 
-def get_trial_session_date(smile_trial: dict) -> str:
+def get_trial_session_date(smile_trial: dict) -> pd.Timestamp:
     return pd.to_datetime(
         datetime.strptime(
             str(int(smile_trial['Overview']['date'])),
@@ -35,25 +34,6 @@ def get_trial_result(smile_trial: dict) -> str:
         }[result_code]
     )
 
-def get_trial_task(smile_trial: dict) -> str:
-    trial_name = smile_trial['Overview']['trialName']
-    if trial_name.startswith('RandomTargetTask'):
-        task = 'RTT'
-    elif trial_name.startswith('CST'):
-        task = 'CST'
-    elif trial_name.startswith('CenterOut'):
-        task = 'CO'
-    elif re.match(r'^R.T.$', trial_name):
-        task = 'DCO'
-    elif re.match(r'^R.T.C$', trial_name):
-        task = 'DCO-catch'
-    elif re.match(r'^memory-guided.*$', trial_name):
-        task = 'DCO'
-    else:
-        task = trial_name
-
-    return task
-
 def get_smile_meta(smile_data: list, **kwargs) -> pd.DataFrame:
     return pd.DataFrame(
         [
@@ -61,7 +41,7 @@ def get_smile_meta(smile_data: list, **kwargs) -> pd.DataFrame:
                 'monkey': smile_trial['Overview']['subjectName'],
                 'session date': get_trial_session_date(smile_trial),
                 'trial datetime': get_trial_datetime(smile_trial),
-                'task': get_trial_task(smile_trial),
+                'trial name': smile_trial['Overview']['trialName'],
                 'result': get_trial_result(smile_trial),
             } | kwargs
             for smile_trial in smile_data
